47d70b2376b08a21eff454d931355d2d8b9a6f96
aritra biswas <pandalearnstocode@gmail.com>
2022-05-18 10:12:31 +0530

    adding a sample hello world function.

---
+++ project/hello_world.py
@@ -0,0 +1,10 @@
+def say_hello_world(name:str)-> str:
+    """Hello world function
+
+    Args:
+        name (str): persons name
+
+    Returns:
+        str: say hello to person
+    """
+    return f"Hello {name}!!!!"
